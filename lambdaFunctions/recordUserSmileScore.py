29fcf547bac019b7c7ce6152c09d32cb28d5dbca
avidubey <avidubey@amazon.com>
2019-02-09 18:09:10 -0800

    Merging recordUserSmileScore

--- lambdaFunctions/recordUserSmileScore.py
+++ lambdaFunctions/recordUserSmileScore.py
@@ -1,14 +1,30 @@
 import boto3
 import json
+import decimal
 
-s3 = boto3.client('s3', region_name='us-west-2')
+s3 = boto3.client('dynamodb', region_name='us-west-2')
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+table = dynamodb.Table('UserSmileScore')
 
 def lambda_handler(event, context):
-    # userId = event["userId"]
-    # smileConfidencePercentage = event["smileConfidence"]
-    # noOfPeopleInPhoto = event["noOfPeopleInPhoto"]
+    userId = event["userId"]
+    smileConfidencePercentage = event["smileConfidence"]
+    noOfPeopleInPhoto = event["noOfPeopleInPhoto"]
     
-    # Shuklaji : algo for scoring, storing in ddb, api for retrieving
+    response = table.update_item(
+    Key={
+        'UserId': userId
+    },
+    UpdateExpression="add score :s, numberOfTimeSeen :c, seenWithPeople :p",
+    ExpressionAttributeValues={
+        ':s': decimal.Decimal(noOfPeopleInPhoto * decimal.Decimal(smileConfidencePercentage/100)).quantize(decimal.Decimal('1.00')),
+        ':c': decimal.Decimal(1),
+        ':p': decimal.Decimal(noOfPeopleInPhoto),
+    })
+    
+
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Success')
